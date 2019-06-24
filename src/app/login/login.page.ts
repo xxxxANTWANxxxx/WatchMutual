8a0d16d0048f812761d5558bee722e702bd38b58
irishlad98 <hme1@mail.usf.edu>
2019-06-24 12:56:22 -0400

    Verification added

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
 import { Router } from '@angular/router';
-declare const OktaAuth: any;
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import * as config from 'config.json';
 
 
 @Component({
@@ -18,10 +18,32 @@ export class LoginPage implements OnInit {
   private password: string;
   private error: string;
 
-  constructor(private navCtrl: NavController, private router: Router) { 
+  constructor(private http: HttpClient, private navCtrl: NavController, private router: Router) { 
   }
 
   ngOnInit() {
+  }
+
+  loginClicked() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      })
+    };
+
+    let postData = {
+      "username": this.username,
+      "password": this.password
+    }
+
+    this.http.post("http://localhost:4200/login", postData, httpOptions)
+      .subscribe(data => {
+        console.log("Post Successful");
+        if(data['Status']) this.login();
+      }, error => {
+        console.log(error)
+      });
   }
 
   login(): void {
